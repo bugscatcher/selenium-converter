e02c69049aff2840977148e45f8eb5b3418e2679
bugscatcher <inurrick@gmail.com>
2016-12-02 23:06:05 +0300

    test commit

--- src/test/java/ru/sberbank/selenium/Basic.java
+++ src/test/java/ru/sberbank/selenium/Basic.java
@@ -4,19 +4,16 @@ import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.util.concurrent.TimeUnit;
 
 public class Basic {
 
     WebDriver driver;
-    String baseUrl = "http://www.sberbank.ru/ru/quotes/converter";
+    private String baseUrl = "http://www.sberbank.ru/ru/quotes/converter";
 
     @Before
     public void setUp() {
-//        TODO
         System.setProperty("webdriver.chrome.driver", "D:\\chromedriver_win32\\chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
